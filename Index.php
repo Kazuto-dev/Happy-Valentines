e9fbcf5696b0c5eff257a25a3c417a34ea73907e
Jandel Isidro <kazutokirigayakirito60@gmail.com>
2023-02-13 07:54:31 +0800

    Iloveyou

---
+++ Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport"content="width=device-width,initial-scale=1.0">
+     <title>Letter for BBii !! <3</title>
+     <link rel="stylesheet"href="style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <div class="title">
+            <h1>Hello Bbi !!</h1>
+            <p>&#128525;</p>
+        </div>
+    <div class="button">
+        <a href="page1.php"><p>Next</p></a>
+    </div>
+
+    </div>
+</body>
+</html>
---
+++ page1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport"content="width=device-width,initial-scale=1.0">
+     <title>Letter for BBii !! <3</title>
+     <link rel="stylesheet"href="style.css">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <div class="message">
+            <p>
+                Hello BBi !! <br>
+                I miss youuu hhehehe hmmm I just want to say na Thank youuuu!! . Thank you kasi nandito ka kasama kita kahit saan and legit yon bbi hihi nakikita ko future ko sayo kaya sure na sure ako na ikaw na talaga. Im hoping in the near future na kaya na nating harapin mga fears natin sa buhay and nandito ako at nandito ka para tulungan natin ang isa't isa, dont worry bbi susuportahan kita and sasamahan kita sa journey mo. Im always here lang, One call away lang me.
+                pasensya kana ha wala ako masyado masabi ang gusto ko lang makasama kita gang sa pag tanda mahal kita tanya sobrang mahal kita kaya sana walang bibitaw satin okay? hihi kahit anong mangyare ^^ mwuah Happy Valentines Day BBi !! <3 heart heart !! &#128525; &#128525; &#128525; Iloveyou.
+            </p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
